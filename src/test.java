e2daf107646cc51738e3700631c2adb2ddfdeaec
gjaku1031 <gjaku1031@gmail.com>
2024-12-26 14:15:51 +0900

    code

--- src/test.java
+++ src/test.java
@@ -1,6 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class test {
 
     public static void main(String[] args) {
+        BufferedReader bt = new BufferedReader(new InputStreamReader(System.in));
 
     }
 }
