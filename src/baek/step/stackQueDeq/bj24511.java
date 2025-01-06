a8b464f21dae39dcb04cc8df203c223644c54ebd
gjaku1031 <gjaku1031@gmail.com>
2025-01-06 09:25:59 +0900

    code

---
+++ src/baek/step/stackQueDeq/bj24511.java
@@ -0,0 +1,14 @@
+package baek.step.stackQueDeq;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj24511 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+    }
+}
