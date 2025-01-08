fcd43687c9ac934369c00efd6cdcc1d8a4c583bb
gjaku1031 <gjaku1031@gmail.com>
2025-01-08 09:13:49 +0900

    code

--- src/baek/setMap/bj10815.java
+++ src/baek/setMap/bj10815.java
@@ -3,12 +3,28 @@ package baek.setMap;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class bj10815 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st1.nextToken()));
+        }
 
+        int M = Integer.parseInt(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            if (set.contains(Integer.parseInt(st2.nextToken()))) {
+                System.out.print(1 + " ");
+            } else {
+                System.out.print(0 + " ");
+            }
+        }
     }
 }
