cfd8b2618859c1c13d671f1c01cd412110d632f7
gjaku1031 <gjaku1031@gmail.com>
2025-01-10 11:45:54 +0900

    code

--- src/baek/step/tree/bj1991.java
+++ src/baek/step/tree/bj1991.java
@@ -16,38 +16,106 @@ public class bj1991 {
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
 
-        chList = new ArrayList[N];
-        for (int i = 0; i < N; i++) {
+        chList = new ArrayList[N + 1];
+
+        for (int i = 0; i <= N; i++) {
             chList[i] = new ArrayList<>();
         }
+        chList[0].add(null);
+        chList[0].add(null);
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
+            chList[i].add(0);
+            chList[i].add(0);
+        }
+
+
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             char a = st.nextToken().charAt(0);
             char b = st.nextToken().charAt(0);
             char c = st.nextToken().charAt(0);
             if (b != '.') {
-                chList[a - 65].add(b - 65);
+                chList[a - 64].set(0, b - 64);
             }
-            if (c != '.') {
-                chList[a - 65].add(c - 65);
+            if (c != '.' && chList[i].get(1) == 0) {
+                chList[a - 64].set(1, c - 64);
             }
         }
-
-        for (ArrayList<Integer> integers : chList) {
-            System.out.println(integers);
-        }
-        preorder(0);
+        preorder(1);
+        System.out.println();
+        inorder(1);
+        System.out.println();
+        postorder(1);
     }
 
-    static void preorder(int n) {
-        if (chList[n].isEmpty()) {
+    static void preorder(int index) {
+        //System.out.println(index + "번 실행");
+        ArrayList<Integer> arr = chList[index];
+        if (arr.get(0) == null && arr.get(1) == null) {
+            //System.out.println(index + " 번 비어서 종료");
             return;
         }
-        System.out.print((char) (n + 65));
-        preorder(chList[n].get(0));
-        if (chList[n].size() > 1) {
-            preorder(chList[n].get(1));
+        System.out.print((char) (index + 64));
+        if (arr.get(0) != null) {
+            preorder(arr.get(0));
+        }else {
+            preorder(0);
         }
+
+        if (arr.get(1) != null) {
+            preorder(arr.get(1));
+        } else {
+            preorder(0);
+        }
+        //System.out.println(index + "번 종료");
+    }
+    static void inorder(int index) {
+        //System.out.println(index + "번 실행");
+        ArrayList<Integer> arr = chList[index];
+
+        if (arr.get(0) == null && arr.get(1) == null) {
+            //System.out.println(index + " 번 비어서 종료");
+            return;
+        }
+
+        if (arr.get(0) != null) {
+            inorder(arr.get(0));
+        }else {
+            inorder(0);
+        }
+
+        System.out.print((char) (index + 64));
+
+        if (arr.get(1) != null) {
+            inorder(arr.get(1));
+        } else {
+            inorder(0);
+        }
+        //System.out.println(index + "번 종료");
+    }
+    static void postorder(int index) {
+        //System.out.println(index + "번 실행");
+        ArrayList<Integer> arr = chList[index];
+
+        if (arr.get(0) == null && arr.get(1) == null) {
+            //System.out.println(index + " 번 비어서 종료");
+            return;
+        }
+
+        if (arr.get(0) != null) {
+            postorder(arr.get(0));
+        }else {
+            postorder(0);
+        }
+
+
+        if (arr.get(1) != null) {
+            postorder(arr.get(1));
+        } else {
+            postorder(0);
+        }
+        System.out.print((char) (index + 64));
+        //System.out.println(index + "번 종료");
     }
 }
