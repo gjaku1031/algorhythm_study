d0f0b4b5ae1010533d0abbeb402e568e33c84e31
gjaku1031 <gjaku1031@gmail.com>
2025-01-13 11:03:50 +0900

    code

---
+++ src/baek/step/graph/bj2178.java
@@ -0,0 +1,14 @@
+package baek.step.graph;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class bj2178 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, M;
+
+
+    public static void main(String[] args) {
+
+    }
+}
