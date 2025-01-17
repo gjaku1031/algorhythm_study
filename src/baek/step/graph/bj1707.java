3ea69b618dbd6f63852b6995380e60de4a0caed5
gjaku1031 <gjaku1031@gmail.com>
2025-01-17 12:46:05 +0900

    code

--- src/baek/step/graph/bj1707.java
+++ src/baek/step/graph/bj1707.java
@@ -10,13 +10,10 @@ import java.util.StringTokenizer;
 
 public class bj1707 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static ArrayList<Node>[] graph;
+    static ArrayList<Integer>[] graph;
     static StringTokenizer st;
+    static int[] visited; // 색도 같이 저장
     static int K, V, E;
-    static Deque<Node> deque = new ArrayDeque<>();
-    static boolean[] visited;
-    static Node[] nodes;
-
 
     public static void main(String[] args) throws IOException {
         K = Integer.parseInt(br.readLine());
@@ -24,89 +21,55 @@ public class bj1707 {
             st = new StringTokenizer(br.readLine());
             V = Integer.parseInt(st.nextToken());
             E = Integer.parseInt(st.nextToken());
-            visited = new boolean[V + 1];
 
-            // 모든 노드 흰색으로 색칠
-            nodes = new Node[V + 1];
-            for (int j = 0; j <= V; j++) {
-                nodes[j] = new Node(j, Color.White);
-            }
-
-            // 노드 연결 시작
+            visited = new int[V + 1];
             graph = new ArrayList[V + 1];
+
             for (int j = 0; j <= V; j++) {
                 graph[j] = new ArrayList<>();
             }
+
             for (int j = 0; j < E; j++) {
                 st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
-                graph[a].add(nodes[b]);
-                graph[b].add(nodes[a]);
+                graph[a].add(b);
+                graph[b].add(a);
             }
-
-            //BFS 세팅
-            visited[1] = true;
-            nodes[1].setColor(Color.Red);
-            deque.add(nodes[1]);
             BFS();
         }
     }
 
-    static class Node {
-        int index;
-        Color color;
-
-        public Node(int index, Color color) {
-            this.index = index;
-            this.color = color;
-        }
-
-        public int getIndex() {
-            return index;
-        }
-
-        public Color getColor() {
-            return color;
-        }
-
-        public void setColor(Color color) {
-            this.color = color;
-        }
-    }
-
-    enum Color {
-        White,
-        Blue,
-        Red,
-        ;
-    }
-
     static void BFS() {
-        while (!deque.isEmpty()) {
-            Node now = deque.pollFirst();
-            int nowIdx = now.getIndex();
-            for (int i = 0; i < graph[nowIdx].size(); i++) {
-                Node next = graph[nowIdx].get(i);
-                int nextIdx = next.getIndex();
-                if (!visited[nextIdx]) {
-                    visited[nextIdx] = true;
-                    deque.addLast(next);
-                    if (now.getColor() == Color.Blue) {
-                        if (next.getColor() == Color.White) {
-                            next.setColor(Color.Red);
-                        } else {
-                            System.out.println("NO");
-                            return;
-                        }
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 1; i <= V; i++) {
+            if (visited[i] == 0) {
+                deque.add(i);
+                visited[i] = 1;
+            }
+            while (!deque.isEmpty()) {
+                int now = deque.pollFirst();
+
+                for (int j = 0; j < graph[now].size(); j++) {
+                    int next = graph[now].get(j);
+
+                    // 첫 방문
+                    if (visited[next] == 0) {
+                        deque.add(next);
                     }
-                    if (now.getColor() == Color.Red) {
-                        if (next.getColor() == Color.White) {
-                            next.setColor(Color.Blue);
-                        } else {
-                            System.out.println("NO");
-                            return;
-                        }
+
+                    // 색 같으면 리턴
+                    if (visited[next] == visited[now]) {
+                        System.out.println("NO");
+                        return;
+                    }
+
+                    // 다른 색 칠함
+                    if (visited[now] == 1 && visited[next] == 0) {
+                        visited[next] = 2;
+                    } else if (visited[now] == 2 && visited[next] == 0) {
+                        visited[next] = 1;
                     }
                 }
             }
