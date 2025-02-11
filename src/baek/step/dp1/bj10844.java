a30e09f1cd5f4312fc32ab242e421059727e028c
gjaku1031 <gjaku1031@gmail.com>
2025-02-11 12:38:42 +0900

    커밋

---
+++ src/baek/step/dp1/bj10844.java
@@ -0,0 +1,56 @@
+package baek.step.dp1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj10844 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N + 1][10];
+        for (int i = 1; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][1];
+                } else if (j == 9) {
+                    dp[i][j] = dp[i - 1][8];
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
+                }
+                dp[i][j] %= 1000000000;
+            }
+        }
+        long result = 0;
+        for (int i = 0; i < 10; i++) {
+            result = (result + dp[N][i]) % 1000000000;
+        }
+        System.out.println(result);
+    }
+}
+/*
+10 12
+21 23
+32 34
+43 45
+54 56
+65 67
+76 78
+87 89
+98
+
+
+101 / 121 123
+210 212 / 232 234
+321 323 / 343 345
+...
+...
+...
+876 878 / 898
+987 989
+*/
