411db5fbde927ca4b4a6174b6713913ee8785491
gjaku1031 <gjaku1031@gmail.com>
2025-02-14 16:41:00 +0900

    커밋

--- src/baek/step/dp1/bj11053.java
+++ src/baek/step/dp1/bj11053.java
@@ -3,6 +3,7 @@ package baek.step.dp1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class bj11053 {
@@ -15,20 +16,35 @@ public class bj11053 {
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        dp = new int[N];
+        arr = new int[N + 1];
+        dp = new int[N + 1];
 
+        //=====초기화=====
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        for (int i = 0; i <= N; i++) {
+            dp[i] = 1;
+        }
+        //=====1일때=====
         if (N == 1) {
             System.out.println(1);
             return;
         }
 
-        max = 1;
+        for (int i = 2; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        Arrays.sort(dp);
+        System.out.println(dp[N]);
+
+
 
 
     }
