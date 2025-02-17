4702b2a80dc2d538e47b59bae329886c877d6abe
gjaku1031 <gjaku1031@gmail.com>
2025-02-17 14:41:11 +0900

    커밋

---
+++ src/baek/step/recursion/bj24060.java
@@ -0,0 +1,27 @@
+package baek.step.recursion;
+
+public class bj24060 {
+
+    public static void main(String[] args) {
+
+    }
+
+/*    int[] mergeSort(int[] arr) {
+        if (arr[0] < arr[arr.length - 1]) {
+        }
+
+    }*/
+
+    void merge(int[] arr, int p, int q, int r) {
+        int i = p;
+        int j = q + 1;
+        int t = 1;
+
+        while (i <= q && j <= r) {
+            if (arr[i] <= arr[j]) {
+
+            }
+        }
+
+    }
+}
