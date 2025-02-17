20a1dc843fe0b78ee79ac23193129b6a546b55a2
gjaku1031 <gjaku1031@gmail.com>
2025-02-17 14:41:16 +0900

    커밋

---
+++ src/baek/bj28702.java
@@ -0,0 +1,46 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj28702 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = 0;
+        int idx = 0;
+        for (int i = 0; i < 3; i++) {
+            String line = br.readLine();
+            if (isNumeric(line)) {
+                a = Integer.parseInt(line);
+                idx = i;
+            }
+        }
+        int result = a + 3 - idx;
+        fizzBuzz(result);
+    }
+
+    static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str); // 정수든 실수든 숫자로 변환 시도
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    static void fizzBuzz(int result) {
+        if (result % 3 == 0 && result % 5 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (result % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (result % 5 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(result);
+        }
+
+    }
+
+}
