66a68f1ff83719035af18f20b2c03e93a3a52231
gjaku1031 <gjaku1031@gmail.com>
2025-02-21 23:56:56 +0900

    커밋

---
+++ src/baek/bj18110.java
@@ -0,0 +1,34 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class bj18110 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] solved;
+    static int N;
+    static int except;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        solved = new int[N];
+        for (int i = 0; i < N; i++) {
+            solved[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(solved);
+        except = (int)Math.round(N * 15 / 100.0);
+
+        for (int i = 0; i < except; i++) {
+            solved[i] = 0;
+            solved[N - 1 - i] = 0;
+        }
+
+        int sum = 0;
+        for (int i : solved) {
+            sum += i;
+        }
+        System.out.println(Math.round(sum / (double)(N - 2 * except)));
+    }
+}
