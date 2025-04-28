5884217a7853af90b1ccef4231dad8a9268ac74f
gjaku1031 <gjaku1031@gmail.com>
2025-04-28 19:38:46 +0900

    sw1954.java 완료

--- src/sw/D2/sw1954.java
+++ src/sw/D2/sw1954.java
@@ -1,82 +1,40 @@
 package sw.D2;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class sw1954 {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int count;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        for (int case_num = 1; case_num <= T; case_num++) {
-
+        for (int tc = 1; tc <= T; tc++) {
             int N = Integer.parseInt(br.readLine());
-            count = 2
-            ;
-            int[][] nums = new int[N][N];
-            nums[0][0] = 1;
+            int[][] arr = new int[N][N];
+            // 우, 하, 좌, 상
+            int[] dx = {0, 1, 0, -1};
+            int[] dy = {1, 0, -1, 0};
+            int x = 0, y = 0, dir = 0;
 
+            for (int num = 1; num <= N * N; num++) {
+                arr[x][y] = num;
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N || arr[nx][ny] != 0) {
+                    dir = (dir + 1) % 4;
+                    nx = x + dx[dir];
+                    ny = y + dy[dir];
+                }
+                x = nx;
+                y = ny;
+            }
 
-            int x = 0;
-            int y = 0;
-
-
-
-
-        }
-
-    }
-
-    //좌상단에서 우향
-    static void snail_1(int[][] nums, int x, int y, int N) {
-        int now = y + 1;
-
-        for (int i = 1; i < N; i++) {
-            if (nums[x][y + i] != 0) {
-                now = y + i;
-                nums[x][now] = count++;
-            } else if (count == N + 1) {
-                return;
-            } else {
-                snail_2(nums, x, now + 1, N);
-                return;
+            System.out.println("#" + tc);
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    System.out.print(arr[i][j] + " ");
+                }
+                System.out.println();
             }
         }
     }
-    //우상단에서 하향
-    static void snail_2(int[][] nums, int x, int y, int N){
-        int now = x + 1;
-
-        for (int i = 1; i < N; i++) {
-            if (nums[x + i][y] != 0) {
-                now = x + i;
-                nums[now][y] = count++;
-            } else if (count == N + 1) {
-                return;
-            } else {
-                snail_3(nums, now + 1, y, N);
-                return;
-            }
-        }
-    }
-
-    //우하단에서 좌향
-    static void snail_3(int[][] nums, int x, int y, int N) {
-        int now = y - 1;
-        for (int i = 1; i < N; i++) {
-            if (nums[x][y - i] != 0) {
-                now = y - i;
-                nums[x][now] = count++;
-            } else if (count == N + 1) {
-                return;
-            } else {
-                snail_2(nums, x, now + 1, N);
-                return;
-            }
-        }
-    }
-
-    //좌하단에서 상향
 }
