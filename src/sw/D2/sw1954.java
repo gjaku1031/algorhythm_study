d23ddfea0fa79cd32e542d71cab9069c522954c0
gjaku1031 <gjaku1031@gmail.com>
2025-04-28 18:14:07 +0900

    sw1954.java

--- src/sw/D2/sw1954.java
+++ src/sw/D2/sw1954.java
@@ -13,26 +13,70 @@ public class sw1954 {
         for (int case_num = 1; case_num <= T; case_num++) {
 
             int N = Integer.parseInt(br.readLine());
-            count = N;
+            count = 2
+            ;
             int[][] nums = new int[N][N];
+            nums[0][0] = 1;
+
 
             int x = 0;
             int y = 0;
 
-            snail(nums, x, y, N);
+
 
 
         }
 
     }
 
-    static void snail(int[][] nums, int x, int y, int N) {
-        if (x==0 && y==0) {
-            for (int i = 1; i <= N; i++) {
-                nums[x][i] = i;
+    //좌상단에서 우향
+    static void snail_1(int[][] nums, int x, int y, int N) {
+        int now = y + 1;
+
+        for (int i = 1; i < N; i++) {
+            if (nums[x][y + i] != 0) {
+                now = y + i;
+                nums[x][now] = count++;
+            } else if (count == N + 1) {
+                return;
+            } else {
+                snail_2(nums, x, now + 1, N);
+                return;
             }
         }
-
-
     }
+    //우상단에서 하향
+    static void snail_2(int[][] nums, int x, int y, int N){
+        int now = x + 1;
+
+        for (int i = 1; i < N; i++) {
+            if (nums[x + i][y] != 0) {
+                now = x + i;
+                nums[now][y] = count++;
+            } else if (count == N + 1) {
+                return;
+            } else {
+                snail_3(nums, now + 1, y, N);
+                return;
+            }
+        }
+    }
+
+    //우하단에서 좌향
+    static void snail_3(int[][] nums, int x, int y, int N) {
+        int now = y - 1;
+        for (int i = 1; i < N; i++) {
+            if (nums[x][y - i] != 0) {
+                now = y - i;
+                nums[x][now] = count++;
+            } else if (count == N + 1) {
+                return;
+            } else {
+                snail_2(nums, x, now + 1, N);
+                return;
+            }
+        }
+    }
+
+    //좌하단에서 상향
 }
