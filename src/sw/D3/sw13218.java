9914a6058941a36e3b00e7dea0d07352b5bcdfc9
gjaku1031 <gjaku1031@gmail.com>
2025-04-30 00:35:36 +0900

    sw13218.java

---
+++ src/sw/D3/sw13218.java
@@ -0,0 +1,19 @@
+package sw.D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class sw13218 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < T; tc++) {
+            
+        }
+
+
+    }
+    
+}
