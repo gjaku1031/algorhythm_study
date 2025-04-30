b6c3e9705198e3c91392a282069efcca0b5d9290
gjaku1031 <gjaku1031@gmail.com>
2025-04-30 17:03:27 +0900

    sw1284.java

---
+++ src/sw/D2/sw1284.java
@@ -0,0 +1,36 @@
+package sw.D2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class sw1284 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            st = new StringTokenizer(br.readLine());
+
+            int P = Integer.parseInt(st.nextToken());
+            int Q = Integer.parseInt(st.nextToken());
+            int R = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+
+            int A = P * W;
+            int B;
+            if (R >= W) {
+                B = Q;
+            } else {
+                B = Q + (W - R) * S;
+            }
+
+            int result = Math.min(A, B);
+            System.out.println("#" + tc + " " + result);
+        }
+    }
+}
