96cd756f9c279a1acf76484c2f3d6e4ec0a4e558
gjaku1031 <gjaku1031@gmail.com>
2025-05-04 00:45:11 +0900

    bj1984.java 개선

--- src/baek/step/dp1/bj9184.java
+++ src/baek/step/dp1/bj9184.java
@@ -1,30 +1,26 @@
 package baek.step.dp1;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class bj9184 {
-    static StringTokenizer st;
+
     static int[][][] w = new int[21][21][21];
 
-
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        w[20][20][20] = 1048576;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // Bottom-up
         for (int i = 0; i < 21; i++) {
             for (int j = 0; j < 21; j++) {
                 for (int k = 0; k < 21; k++) {
+
                     if (i == 0 || j == 0 || k == 0) {
                         w[i][j][k] = 1;
-                    }
-                }
-            }
-        }
-        for (int i = 1; i < 21; i++) {
-            for (int j = 1; j < 21; j++) {
-                for (int k = 1; k < 21; k++) {
-                    if (i < j && j < k) {
+                    } else if (i < j && j < k) {
                         w[i][j][k] = w[i][j][k - 1] + w[i][j - 1][k - 1] - w[i][j - 1][k];
                     } else {
                         w[i][j][k] = w[i - 1][j][k] + w[i - 1][j - 1][k] + w[i - 1][j][k - 1] - w[i - 1][j - 1][k - 1];
@@ -33,21 +29,35 @@ public class bj9184 {
             }
         }
 
-        while (sc.hasNext()) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            // 종료 조건
             if (a == -1 && b == -1 && c == -1) {
                 break;
             }
-            if (a <= 0 || b <= 0 || c <= 0) {
-                System.out.println("w(" + a + ", " + b + ", " + c + ") = " + 1);
-            } else if (a > 20 || b > 20 || c > 20) {
-                System.out.println("w(" + a + ", " + b + ", " + c + ") = " + 1048576);
 
+            int result;
+            // 입력 값에 따른 결과 결정
+            if (a <= 0 || b <= 0 || c <= 0) {
+                result = 1;
+            } else if (a > 20 || b > 20 || c > 20) {
+                result = w[20][20][20];
             } else {
-                System.out.println("w(" + a + ", " + b + ", " + c + ") = " + w[a][b][c]);
+                result = w[a][b][c];
             }
+
+            sb.append("w(").append(a)
+                    .append(", ").append(b)
+                    .append(", ").append(c)
+                    .append(") = ")
+                    .append(result)
+                    .append("\n");
         }
+
+        System.out.print(sb);
     }
 }
