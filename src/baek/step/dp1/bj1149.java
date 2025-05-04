a254384d4f2c727bdcab3b6b4e7033a072f49991
gjaku1031 <gjaku1031@gmail.com>
2025-05-04 17:58:32 +0900

    bj1149.java 공간복잡도 개선

--- src/baek/step/dp1/bj1149.java
+++ src/baek/step/dp1/bj1149.java
@@ -3,7 +3,6 @@ package baek.step.dp1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class bj1149 {
@@ -11,14 +10,12 @@ public class bj1149 {
     static StringTokenizer st;
     static int[][] cost;
     static int N;
-    static int[][] home;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
         cost = new int[N][3];
-        home = new int[N][3];
 
-        //입력
+        // 입력
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
 
@@ -28,30 +25,22 @@ public class bj1149 {
             }
         }
 
-
-        for (int i = 0; i < 9; i++) {
-
-        }
-
-        // home[집순서][0:R, 1:G, 2:B]
-        home[0][0] = cost[0][0];
-        home[0][1] = cost[0][1];
-        home[0][2] = cost[0][2];
+        // 초기값
+        int prevR = cost[0][0];
+        int prevG = cost[0][1];
+        int prevB = cost[0][2];
 
         for (int i = 1; i < N; i++) {
-            for (int j = 0; j < 3; j++) {
-                int min = 0;
-                if (j == 0) {
-                    min = Math.min(home[i - 1][1], home[i - 1][2]);
-                } else if (j == 1) {
-                    min = Math.min(home[i - 1][0], home[i - 1][2]);
-                } else if (j == 2) {
-                    min = Math.min(home[i - 1][0], home[i - 1][1]);
-                }
-                home[i][j] = cost[i][j] + min;
-            }
+            int currentR = cost[i][0] + Math.min(prevG, prevB);
+            int currentG = cost[i][1] + Math.min(prevR, prevB);
+            int currentB = cost[i][2] + Math.min(prevR, prevG);
+
+            prevR = currentR;
+            prevG = currentG;
+            prevB = currentB;
         }
-        Arrays.sort(home[N - 1]);
-        System.out.println(home[N - 1][0]);
+
+        int result = Math.min(prevR, Math.min(prevG, prevB));
+        System.out.println(result);
     }
 }
