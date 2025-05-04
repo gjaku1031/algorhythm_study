fb7ffc6a518a076bba237e34a9b6e256dbb1180f
gjaku1031 <gjaku1031@gmail.com>
2025-05-05 00:03:20 +0900

    bj1463.java 개선

--- src/baek/step/dp1/bj1463.java
+++ src/baek/step/dp1/bj1463.java
@@ -5,37 +5,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class bj1463 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(br.readLine());
-        int[] count = new int[1000001];
 
-        count[1] = 0;
-        count[2] = 1;
-        count[3] = 1;
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
 
-        for (int i = 4; i < 1000001; i++) {
-            if (i % 3 == 0 && i % 2 == 0) {
-                count[i] = min(count[i / 3], count[i / 2], count[i - 1]) + 1;
-            } else if (i % 3 == 0) {
-                count[i] = Math.min(count[i / 3], count[i - 1]) + 1;
-            } else if (i % 2 == 0) {
-                count[i] = Math.min(count[i / 2], count[i - 1]) + 1;
-            } else {
-                count[i] = count[i - 1] + 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             }
         }
-        System.out.println(count[a]);
-        // 10 -> 5 -> 4 -> 2 -> 1
-        // 10 -> 5 -> 4 -> 3 ->1
-        // 10 -> 9 -> 3 -> 1
 
+        System.out.println(dp[N]);
     }
-
-    static int min(int a, int b, int c) {
-        int temp = Math.min(a, b);
-        return Math.min(temp, c);
-    }
-
 }
