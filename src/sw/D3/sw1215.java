123601667affb7a3863d4c53ef6a0d6581d8f617
gjaku1031 <gjaku1031@gmail.com>
2025-05-08 01:19:28 +0900

    sw1215 최종

--- src/sw/D3/sw1215.java
+++ src/sw/D3/sw1215.java
@@ -8,51 +8,62 @@ public class sw1215 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int N;
     static char[][] pal;
+    static int count;
 
     public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
         for (int tc = 1; tc <= 10; tc++) {
+            N = Integer.parseInt(br.readLine());
+            count = 0;
 
-            //입력
+            // 글자판 입력
             pal = new char[8][8];
             for (int i = 0; i < 8; i++) {
-                String str = br.readLine();
+                String strInput = br.readLine().trim();
                 for (int j = 0; j < 8; j++) {
-                    pal[i][j] = str.charAt(i);
+                    pal[i][j] = strInput.charAt(j);
                 }
             }
-
-            int result = 0;
 
             for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 8; j++) {
-
+                    solve(i, j);
                 }
             }
-
-
-
-
-
-
-
+            System.out.println("#" + tc + " " + count);
         }
     }
 
-    static void solve() {
+    static void solve(int r, int c) {
+        // 가로
+        if (c + N <= 8) {
+            StringBuilder sbHorizontal = new StringBuilder();
+            for (int k = 0; k < N; k++) {
+                sbHorizontal.append(pal[r][c + k]);
+            }
+            if (isPal(sbHorizontal.toString())) {
+                count++;
+            }
+        }
 
+        // 세로
+        if (r + N <= 8) {
+            StringBuilder sbVertical = new StringBuilder();
+            for (int k = 0; k < N; k++) {
+                sbVertical.append(pal[r + k][c]);
+            }
+            if (isPal(sbVertical.toString())) {
+                count++;
+            }
+        }
     }
 
     static boolean isPal(String str) {
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+        int len = str.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (str.charAt(i) != str.charAt(len - 1 - i)) {
                 return false;
             }
         }
         return true;
-    }
-
-    static boolean isValid(int r, int c) {
-        return r >= 0 && r < 8 && c >= 0 && c < 8;
     }
 }
