8dbe4fe71c5c18480615b0d1f269a31049946b76
gjaku1031 <gjaku1031@gmail.com>
2025-05-08 09:37:58 +0900

    bj1697.java 개선

--- src/baek/step/graph/bj1697.java
+++ src/baek/step/graph/bj1697.java
@@ -5,12 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class bj1697 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static HashSet<Integer> visited = new HashSet<>();
+    static boolean[] visited = new boolean[100001];
     static Deque<Node> deq = new ArrayDeque<>();
     static StringTokenizer st;
     static int N, K;
@@ -19,14 +18,21 @@ public class bj1697 {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
+
+        if (N == K) {
+            System.out.println(0);
+            return;
+        }
+
         Node node = new Node(N, 0);
         deq.addLast(node);
-        visited.add(node.getLocation());
+        visited[node.getLocation()] = true;
         BFS();
     }
 
     static class Node {
         int location;
+        int level;
 
         public int getLocation() {
             return location;
@@ -35,8 +41,6 @@ public class bj1697 {
         public int getLevel() {
             return level;
         }
-
-        int level;
 
         public Node(int location, int level) {
             this.location = location;
@@ -50,20 +54,19 @@ public class bj1697 {
         int back = now.getLocation() - 1;
         int teleport = now.getLocation() * 2;
 
-        if (front <= 100000 && !visited.contains(front)) {
+        if (front <= 100000 && !visited[front]) {
             deq.add(new Node(front, nowLevel + 1));
-            visited.add(front);
+            visited[front] = true;
         }
-        if (back <= 100000 && !visited.contains(back)) {
+        if (back >= 0 && back <= 100000 && !visited[back]) {
             deq.add(new Node(back, nowLevel + 1));
-            visited.add(back);
+            visited[back] = true;
         }
-        if (teleport <= 100000 && !visited.contains(teleport)) {
+        if (teleport <= 100000 && !visited[teleport]) {
             deq.add(new Node(teleport, nowLevel + 1));
-            visited.add(teleport);
+            visited[teleport] = true;
         }
     }
-
 
     static void BFS() {
         while (!deq.isEmpty()) {
