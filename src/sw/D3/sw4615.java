833e1cc942fd25892f7c2045ee0408ede823d862
gjaku1031 <gjaku1031@gmail.com>
2025-05-11 00:48:42 +0900

    sw4615.java

---
+++ src/sw/D3/sw4615.java
@@ -0,0 +1,63 @@
+package sw.D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class sw4615 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int T, N, M;
+    static int[][] board;
+
+    //상, 하, 좌, 우, 좌상, 우상, 좌하, 우하
+    static int[] dc = {1, -1, 0, 0, 1, 1, -1, -1};
+    static int[] dr = {0, 0, -1, 1, -1, 1, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+
+            init();
+
+
+        }
+    }
+
+
+    static void init() {
+        board = new int[N][N];
+        board[N / 2][N / 2] = 2;
+        board[N / 2 - 1][N / 2] = 1;
+        board[N / 2][N / 2 - 1] = 1;
+        board[N / 2 - 1][N / 2 - 1] = 2;
+    }
+
+    static void othello() throws IOException {
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int c = Integer.parseInt(st.nextToken()) - 1;
+            int r = Integer.parseInt(st.nextToken()) - 1;
+            int color = Integer.parseInt(st.nextToken());
+            board[c][r] = color;
+
+            for (int j = 0; j < 8; j++) {
+                int nc = c + dc[j];
+                int nr = r + dr[j];
+                if (valid(nc, nr) && board[nc][nr] + board[c][r] == 3) {
+
+                }
+            }
+        }
+    }
+
+    static boolean valid(int nc, int nr) {
+        return nc >= 0 || nc < N || nr >= 0 || nr < N;
+    }
+}
