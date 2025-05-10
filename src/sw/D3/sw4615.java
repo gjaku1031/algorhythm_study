ecfa76a084b0c0ed7d78556ed5c036bb17c46f26
gjaku1031 <gjaku1031@gmail.com>
2025-05-11 02:13:21 +0900

    sw4615

--- src/sw/D3/sw4615.java
+++ src/sw/D3/sw4615.java
@@ -12,9 +12,11 @@ public class sw4615 {
     static int T, N, M;
     static int[][] board;
 
-    //상, 하, 좌, 우, 좌상, 우상, 좌하, 우하
-    static int[] dc = {1, -1, 0, 0, 1, 1, -1, -1};
-    static int[] dr = {0, 0, -1, 1, -1, 1, -1, 1};
+    static int c, r, color;
+
+    // 상, 하, 좌, 우, 좌상, 우상, 좌하, 우하
+    static int[] dc = { 1, -1, 0, 0, 1, 1, -1, -1 };
+    static int[] dr = { 0, 0, -1, 1, -1, 1, -1, 1 };
 
     public static void main(String[] args) throws IOException {
         T = Integer.parseInt(br.readLine());
@@ -25,13 +27,12 @@ public class sw4615 {
 
             init();
 
-
         }
     }
 
-
     static void init() {
         board = new int[N][N];
+
         board[N / 2][N / 2] = 2;
         board[N / 2 - 1][N / 2] = 1;
         board[N / 2][N / 2 - 1] = 1;
@@ -42,19 +43,30 @@ public class sw4615 {
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int c = Integer.parseInt(st.nextToken()) - 1;
-            int r = Integer.parseInt(st.nextToken()) - 1;
-            int color = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken()) - 1;
+            r = Integer.parseInt(st.nextToken()) - 1;
+            color = Integer.parseInt(st.nextToken());
             board[c][r] = color;
 
             for (int j = 0; j < 8; j++) {
                 int nc = c + dc[j];
                 int nr = r + dr[j];
-                if (valid(nc, nr) && board[nc][nr] + board[c][r] == 3) {
 
+            }
+        }
+    }
+
+    static void reverse(int nc, int nr) {
+        if (valid(nc, nr) && board[nc][nr] + board[c][r] == 3) {
+            int current_c = nc;
+            int current_r = nr;
+            while (true) {
+                if (valid(current_c, current_r)) {
+                    return;
                 }
             }
         }
+
     }
 
     static boolean valid(int nc, int nr) {
