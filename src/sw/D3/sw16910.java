2948a2a7fb02308130446c2b726172d1c65eafa2
gjaku1031 <gjaku1031@gmail.com>
2025-05-16 23:09:21 +0900

    sw16910.java

--- src/sw/D3/sw16910.java
+++ src/sw/D3/sw16910.java
@@ -13,8 +13,17 @@ public class sw16910 {
     public static void main(String[] args) throws IOException {
         T = Integer.parseInt(br.readLine());
         for (int tc = 1; tc <= T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            int result = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (Math.pow(i, 2) + Math.pow(j, 2) <= Math.pow(N, 2)) {
+                        result++;
+                    }
+                }
+            }
 
-            System.out.println("#" + tc + " ");
+            System.out.println("#" + tc + " " + (result * 4 - 4 * N + 5));
         }
     }
 }
