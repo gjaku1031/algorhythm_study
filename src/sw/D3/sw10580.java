9fb829679cd5a555b551f3cf22ca65d56f74c2e1
gjaku1031 <gjaku1031@gmail.com>
2025-05-19 07:28:13 +0900

    sw10580.java

--- src/sw/D3/sw10580.java
+++ src/sw/D3/sw10580.java
@@ -5,34 +5,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class sw11285 { // 클래스 이름 변경 (예시)
+public class sw10580 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static int T;
+    static int[] arr1;
+    static int[] arr2;
 
     public static void main(String[] args) throws IOException {
         T = Integer.parseInt(br.readLine());
         for (int tc = 1; tc <= T; tc++) {
             int N = Integer.parseInt(br.readLine());
-            int totalScore = 0;
+            arr1 = new int[N];
+            arr2 = new int[N];
 
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-
-                double r = Math.sqrt((long)x * x + (long)y * y);
-
-                int dartScore;
-
-                if (x == 0 && y == 0) {
-                    dartScore = 10;
-                } else {
-                    dartScore = Math.max(0, 11 - (int)Math.ceil(r / 20.0));
-                }
-                totalScore += dartScore;
+                arr1[i] = Integer.parseInt(st.nextToken());
+                arr2[i] = Integer.parseInt(st.nextToken());
             }
-            System.out.println("#" + tc + " " + totalScore);
+
+            int result = 0;
+            for (int i = 0; i < N - 1; i++) {
+                for (int j = i + 1; j < N; j++) {
+                    if ((arr1[i] - arr1[j]) * (arr2[i] - arr2[j]) < 0) {
+                        result++;
+                    }
+                }
+            }
+            System.out.println("#" + tc + " " + result);
         }
     }
+
 }
