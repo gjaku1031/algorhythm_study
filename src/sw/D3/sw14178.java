8b5ce77322806945f8c4e99c985849a7e6ec3ff1
gjaku1031 <gjaku1031@gmail.com>
2025-05-18 18:12:26 +0900

    sw14178.java

---
+++ src/sw/D3/sw14178.java
@@ -0,0 +1,31 @@
+package sw.D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class sw14178 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int T;
+
+    public static void main(String[] args) throws IOException {
+        T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int D = Integer.parseInt(st.nextToken());
+
+            int range = 2 * D + 1;
+            int result;
+            if (N % range == 0) {
+                result = N / range;
+            } else {
+                result = N / range + 1;
+            }
+            System.out.println("#" + tc + " " + result);
+        }
+    }
+
+}
