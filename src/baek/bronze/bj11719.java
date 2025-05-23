51cb856f1b0d296c86509ac31f83398079aa2009
gjaku1031 <gjaku1031@gmail.com>
2025-05-23 23:21:13 +0900

    bj11719.java

---
+++ src/baek/bronze/bj11719.java
@@ -0,0 +1,16 @@
+package baek.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj11719 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        String s;
+        while ((s = br.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
+}
