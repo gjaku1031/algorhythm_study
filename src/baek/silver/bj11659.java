51585e480c5cb5435b42a488e6c3340297449a48
gjaku1031 <gjaku1031@gmail.com>
2025-05-29 08:13:28 +0900

    bj11659.java

--- src/baek/silver/bj11659.java
+++ src/baek/silver/bj11659.java
@@ -1,4 +1,43 @@
 package baek.silver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class bj11659 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int N, M;
+    static int[] arr;
+    static int[] sum;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        sum = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            //구간합 배열
+            sum[i] = i == 0 ? arr[i] : sum[i - 1] + arr[i];
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken()) - 1;
+            int end = Integer.parseInt(st.nextToken()) - 1;
+            int result = 0;
+            if (start == 0) {
+                result = sum[end];
+            } else {
+                result = sum[end] - sum[start - 1];
+            }
+            System.out.println(result);
+        }
+    }
 }
