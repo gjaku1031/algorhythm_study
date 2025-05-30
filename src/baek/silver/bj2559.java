f4e9393fc1a120b72fca7b89e205769a9b523e1e
gjaku1031 <gjaku1031@gmail.com>
2025-05-30 10:03:23 +0900

    bj2559.java

---
+++ src/baek/silver/bj2559.java
@@ -0,0 +1,39 @@
+package baek.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj2559 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        for (int i = 0; i < K; i++) {
+            sum += arr[i];
+        }
+
+        int max = sum;
+
+        for (int i = K; i < N; i++) {
+            sum = sum + arr[i] - arr[i - K];
+            if (sum > max) {
+                max = sum;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
