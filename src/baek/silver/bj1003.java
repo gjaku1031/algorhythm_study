787a68848c4116e86d31af9cd5457049911c87fd
gjaku1031 <gjaku1031@gmail.com>
2025-06-03 07:45:21 +0900

    bj11727

--- src/baek/silver/bj1003.java
+++ src/baek/silver/bj1003.java
@@ -18,8 +18,8 @@ public class bj1003 {
         dp[1][1] = 1;
 
         for (int i = 2; i <= 40; i++) {
-            dp[i][0] = dp[i-1][0] + dp[i-2][0];
-            dp[i][1] = dp[i-1][1] + dp[i-2][1];
+            dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+            dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
         }
 
         while (T-- > 0) {
---
+++ src/baek/silver/bj11727.java
@@ -0,0 +1,21 @@
+package baek.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj11727 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] dp = new int[1001];
+
+    public static void main(String[] args) throws IOException {
+        dp[1] = 1;
+        dp[2] = 3;
+        for (int i = 3; i <= 1000; i++) {
+            dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007;
+        }
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(dp[N]);
+
+    }
+}
