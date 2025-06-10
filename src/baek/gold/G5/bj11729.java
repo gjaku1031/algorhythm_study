a1150228d30be09407397fed5be4423f436e11b9
gjaku1031 <gjaku1031@gmail.com>
2025-06-10 21:45:48 +0900

    bj11729.java

--- src/baek/gold/G5/bj11729.java
+++ src/baek/gold/G5/bj11729.java
@@ -9,24 +9,23 @@ public class bj11729 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static int N;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        if (N % 2 == 0) {
-            hanoi(N, 2);
-        } else {
-            hanoi(N, 3);
-        }
+        System.out.println((1 << N) - 1); //비트 연산
+        hanoi(N, 1, 3, 2);
+        System.out.println(sb);
     }
 
-    static void hanoi(int n, int to) {
-        if (n == 0) {
+    static void hanoi(int n, int from, int to, int via) {
+        if (n == 1) {
+            sb.append(from).append(" ").append(to).append("\n");
             return;
         }
-        if (to == 2) {
-            hanoi(n - 1, 3);
-        } else {
-            hanoi(n - 1, 2);
-        }
+
+        hanoi(n - 1, from, via, to);
+        sb.append(from).append(" ").append(to).append("\n");
+        hanoi(n - 1, via, to, from);
     }
 }
