a889a8f580288a970c514de03a6ae7af41c93daf
gjaku1031 <gjaku1031@gmail.com>
2025-06-28 23:44:09 +0900

    bj5525.java

---
+++ src/baek/silver/S1/bj5525.java
@@ -0,0 +1,20 @@
+package baek.silver.S1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj5525 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static long N, M;
+    static String P;
+
+    public static void main(String[] args) throws IOException {
+        N = Long.parseLong(br.readLine());
+        M = Long.parseLong(br.readLine());
+        P = br.readLine();
+        for (long i = 0; i < M - (2 * N + 1); i++) {
+
+        }
+    }
+}
