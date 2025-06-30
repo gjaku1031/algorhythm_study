8434d4801b614ee196dae8302f79f03a42153f6c
gjaku1031 <gjaku1031@gmail.com>
2025-07-01 03:16:39 +0900

    bj2522.java

--- src/baek/bronze/B3/bj2522.java
+++ src/baek/bronze/B3/bj2522.java
@@ -1,4 +1,31 @@
 package baek.bronze.B3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class bj2522 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            for (int j = N - i - 1; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = N - 1 - i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
