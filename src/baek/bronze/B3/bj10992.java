1bae8521dada05b38fcc80cc5ab7d0bf4f7cdb44
gjaku1031 <gjaku1031@gmail.com>
2025-07-02 16:57:08 +0900

    bj10992.java

---
+++ src/baek/bronze/B3/bj10992.java
@@ -0,0 +1,33 @@
+package baek.bronze.B3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj10992 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            for (int j = N-i-1; j > 0; j--) {
+                System.out.print(" ");
+            }
+            if (i != N - 1) {
+                for (int j = 0; j < 2 * i + 1; j++) {
+                    if (j == 0 || j == 2 * i) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            } else {
+                for (int j = 0; j < 2 * i + 1; j++) {
+                    System.out.print("*");
+                }
+            }
+
+        }
+    }
+}
