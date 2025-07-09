6938c39c3abbbc8808146657f6969559d164bc78
gjaku1031 <gjaku1031@gmail.com>
2025-07-09 23:21:30 +0900

    bj15633.java

--- src/baek/bronze/B3/bj15633.java
+++ src/baek/bronze/B3/bj15633.java
@@ -4,10 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class bj20944 {
+public class bj15633 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
-        int N = Integer.parseInt(br.readLine());
-        System.out.println("a".repeat(Math.max(0, N)));
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum * 5 - 24);
     }
 }
