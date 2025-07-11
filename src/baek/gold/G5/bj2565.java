56380e164514046d6d20812176a284197fa79346
gjaku1031 <gjaku1031@gmail.com>
2025-07-11 20:12:33 +0900

    bj2565.java

--- src/baek/gold/G5/bj2565.java
+++ src/baek/gold/G5/bj2565.java
@@ -3,13 +3,37 @@ package baek.gold.G5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class bj2565 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
+    static int N;
+    static int[][] arr;
+    static int[] dp;
+
     public static void main(String[] args) throws IOException {
-        
-        
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+
+        }
+        dp = new int[N];
+        Arrays.sort(arr, (o1, o2) -> o1[0] - o2[0]);
+
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i][1] > arr[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        Arrays.sort(dp);
+        System.out.println(N - dp[N - 1]);
     }
 }
