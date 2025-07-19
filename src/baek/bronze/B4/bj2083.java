086e427e8f908db72197159f0a344ff9f54b7f87
gjaku1031 <gjaku1031@gmail.com>
2025-07-19 23:46:16 +0900

    bj2083.java

---
+++ src/baek/bronze/B4/bj2083.java
@@ -0,0 +1,27 @@
+package baek.bronze.B4;
+
+import java.util.Scanner;
+
+public class bj2083 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            String name = scanner.next();
+            int age = scanner.nextInt();
+            int weight = scanner.nextInt();
+            
+            if (name.equals("#") && age == 0 && weight == 0) {
+                break;
+            }
+            
+            if (age > 17 || weight >= 80) {
+                System.out.println(name + " Senior");
+            } else {
+                System.out.println(name + " Junior");
+            }
+        }
+        
+        scanner.close();
+    }
+}
