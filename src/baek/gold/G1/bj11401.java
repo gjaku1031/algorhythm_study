dca0846c1b32b2e70f3d1a5cc2d3cf67229ba8da
gjaku1031 <gjaku1031@gmail.com>
2025-07-23 23:38:40 +0900

    bj2754.java

---
+++ src/baek/gold/G1/bj11401.java
@@ -0,0 +1,14 @@
+package baek.gold.G1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj11401 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) {
+
+    }
+}
