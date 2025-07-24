eadf11dd5b12496ba5aa230d601bda5397d41d3a
Choi <gjaku1031@gmail.com>
2025-07-24 09:27:00 +0900

    bj11444.java

--- src/baek/gold/G2/bj11444.java
+++ src/baek/gold/G2/bj11444.java
@@ -6,29 +6,36 @@ import java.io.InputStreamReader;
 
 public class bj11444 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int N;
+    static long N;
+    static long[] fibo = { 1, 1, 1, 0 };
 
     public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-
-
+        N = Long.parseLong(br.readLine());
+        long[] result = recur(fibo, N);
+        System.out.println(result[2]);
     }
 
-    static void recur(int[] mat, int n) {
+    static long[] recur(long[] mat, long n) {
+        if (n == 1) {
+            return mat;
+        }
 
+        long[] now = recur(mat, n / 2);
+        long[] squared = multi(now, now);
+        if (n % 2 == 1) {
+            return multi(squared, fibo);
+        }
+        return squared;
     }
 
-    static void multi(int[] mat) {
-        int[] next_mat = new int[4];
-        next_mat[0] = mat[0] * mat[0] + mat[1] * mat[2];
-        next_mat[1] = mat[0] * mat[1] + mat[2] * mat[3];
-        next_mat[2] = mat[1] * mat[1] + mat[3] * mat[3];
-        next_mat[3] = mat[0] * mat[3] + mat[2] * mat[1];
+    static long[] multi(long[] mat1, long[] mat2) {
+        long[] next_mat = new long[4];
+        next_mat[0] = (mat1[0] * mat2[0] + mat1[1] * mat2[2]) % 1000000007;
+        next_mat[1] = (mat1[0] * mat2[1] + mat1[1] * mat2[3]) % 1000000007;
+        next_mat[2] = (mat1[2] * mat2[0] + mat1[3] * mat2[2]) % 1000000007;
+        next_mat[3] = (mat1[2] * mat2[1] + mat1[3] * mat2[3]) % 1000000007;
+        return next_mat;
     }
 }
-/*
-an an+1  1 1    an + an+1 a
-0  0     1 2
- */
 
 
