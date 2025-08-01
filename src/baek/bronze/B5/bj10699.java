f6668043c751eb2da12c8ad840f59b61040a554b
gjaku1031 <gjaku1031@gmail.com>
2025-08-01 23:12:46 +0900

    bj10699.java

---
+++ src/baek/bronze/B5/bj10699.java
@@ -0,0 +1,9 @@
+package baek.bronze.B5;
+
+import java.time.LocalDate;
+
+public class bj10699 {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+    }
+}
