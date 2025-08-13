a07d0aa4d7dc2d0c259f2c76b315e25de96ec92a
gjaku1031 <gjaku1031@gmail.com>
2025-08-13 13:37:21 +0900

    bj6987.java

--- src/baek/gold/G4/bj6987.java
+++ src/baek/gold/G4/bj6987.java
@@ -10,40 +10,86 @@ public class bj6987 {
     static StringTokenizer st;
 
     static int[][] team;
-    static boolean[] visited;
-    static int[] order;
+    static StringBuilder result = new StringBuilder();
+    static int answer;
 
     public static void main(String[] args) throws IOException {
+
         for (int tc = 0; tc < 4; tc++) {
             // 입력
             st = new StringTokenizer(br.readLine());
-
             team = new int[6][3];
+            answer = 0;
+
             for (int i = 0; i < 6; i++) {
                 team[i][0] = Integer.parseInt(st.nextToken());
                 team[i][1] = Integer.parseInt(st.nextToken());
                 team[i][2] = Integer.parseInt(st.nextToken());
+
             }
 
+            // 승 + 무 + 패 합이 5아니면 백트래킹 안함
+            boolean flag = true;
+            for (int i = 0; i < 6; i++) {
+                if (team[i][0] + team[i][1] + team[i][2] != 5) {
+                    flag = false;
+                    break;
+                }
+            }
             // 백트래킹
-            visited = new boolean[6];
-            order = new int[6];
-            bt(0);
+            if (flag) {
+                bt(0, 1);
+            }
+            result.append(answer + " ");
         }
+        System.out.println(result);
     }
 
-    static void bt(int level) {
-        if (level == 6) {
+
+    static void bt(int idx, int next) {
+        if (answer == 1) {
             return;
         }
 
-        for (int i = 0; i < 6; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                order[level] = i;
-                bt(level + 1);
-                visited[i] = false;
+        if (idx == 5) {
+            answer = 1;
+            return;
+        }
+
+        if (team[idx][0] > 0 && team[next][2] > 0) {
+            team[idx][0]--;
+            team[next][2]--;
+            if (next == 5) {
+                bt(idx + 1, idx + 2);
+            } else {
+                bt(idx, next + 1);
             }
+
+            team[idx][0]++;
+            team[next][2]++;
+        }
+        if (team[idx][1] > 0 && team[next][1] > 0) {
+            team[idx][1]--;
+            team[next][1]--;
+            if (next == 5) {
+                bt(idx + 1, idx + 2);
+            } else {
+                bt(idx, next + 1);
+            }
+
+            team[idx][1]++;
+            team[next][1]++;
+        }
+        if (team[idx][2] > 0 && team[next][0] > 0) {
+            team[idx][2]--;
+            team[next][0]--;
+            if (next == 5) {
+                bt(idx + 1, idx + 2);
+            } else {
+                bt(idx, next + 1);
+            }
+            team[idx][2]++;
+            team[next][0]++;
         }
     }
 }
