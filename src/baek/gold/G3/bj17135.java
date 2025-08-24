da543ae70dd0492d0ecdc91e90959d0885f35f3b
gjaku1031 <gjaku1031@gmail.com>
2025-08-25 08:50:09 +0900

    bj17135.java

--- src/baek/gold/G3/bj17135.java
+++ src/baek/gold/G3/bj17135.java
@@ -1,6 +1,6 @@
 package baek.gold.G3;
 
-import java.io.BufferedReader;
+import  java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
