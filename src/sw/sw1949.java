2def6edbc638462adcd59360ea4ed8fda2d5f3c7
gjaku1031 <gjaku1031@gmail.com>
2025-09-07 14:22:38 +0900

    sw1929.java

--- src/sw/sw1949.java
+++ src/sw/sw1949.java
@@ -10,7 +10,7 @@ public class sw1949 {
     static int[][] map;
     static boolean[][] visited;
     static int maxLength;
-    static int[] dr = {-1, 1, 0, 0}; // 상, 하, 좌, 우
+    static int[] dr = {-1, 1, 0, 0};
     static int[] dc = {0, 0, -1, 1};
 
     public static void main(String[] args) throws Exception {
@@ -58,20 +58,18 @@ public class sw1949 {
             int nc = c + dc[i];
 
             if (nr >= 0 && nr < N && nc >= 0 && nc < N && !visited[nr][nc]) {
-                // 1. 다음 위치가 현재 위치보다 낮은 경우
                 if (map[nr][nc] < map[r][c]) {
                     dfs(nr, nc, length + 1, hasCut);
                 }
-                // 2. 다음 위치가 현재 위치보다 높거나 같지만, 아직 깎지 않았고 깎아서 낮게 만들 수 있는 경우
                 else if (!hasCut && map[nr][nc] - K < map[r][c]) {
-                    int originalHeight = map[nr][nc]; // 원래 높이 저장
-                    map[nr][nc] = map[r][c] - 1; // 현재보다 1 낮은 높이로 깎음
+                    int originalHeight = map[nr][nc];
+                    map[nr][nc] = map[r][c] - 1;
                     dfs(nr, nc, length + 1, true);
-                    map[nr][nc] = originalHeight; // 높이 복원 (백트래킹)
+                    map[nr][nc] = originalHeight;
                 }
             }
         }
 
-        visited[r][c] = false; // 다른 경로에서 방문할 수 있도록 방문 여부 해제
+        visited[r][c] = false;
     }
 }
