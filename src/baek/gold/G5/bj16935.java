5d74b15313c2b0e6b29fc43e350d4d56f8032625
gjaku1031 <gjaku1031@gmail.com>
2025-09-08 09:39:51 +0900

    bj16935.java

--- src/baek/gold/G5/bj16935.java
+++ src/baek/gold/G5/bj16935.java
@@ -3,7 +3,6 @@ package baek.gold.G5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class bj16935 {
