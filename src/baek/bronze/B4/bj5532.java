c1ab2696efd8432d314c9a66703ae5bb1f462fc4
gjaku1031 <gjaku1031@gmail.com>
2025-10-03 23:24:52 +0900

    bj5532.java

---
+++ src/baek/bronze/B4/bj5532.java
@@ -0,0 +1,26 @@
+package baek.bronze.B4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj5532 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int L = Integer.parseInt(br.readLine()); 
+        int A = Integer.parseInt(br.readLine()); 
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine()); 
+
+        int koreanDays = (A + C - 1) / C;
+
+        int mathDays = (B + D - 1) / D;
+
+        int homeworkDays = Math.max(koreanDays, mathDays);
+
+        int playDays = L - homeworkDays;
+
+        System.out.println(playDays);
+    }
+}
