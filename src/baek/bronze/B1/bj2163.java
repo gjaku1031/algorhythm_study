ea7da1f25e59d199767bb4f8cabb8c495ca8e5e5
gjaku1031 <gjaku1031@gmail.com>
2025-10-12 23:13:36 +0900

    bj2163.java

---
+++ src/baek/bronze/B1/bj2163.java
@@ -0,0 +1,17 @@
+package baek.bronze.B1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj2163 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+
+        System.out.println(n * m - 1);
+    }
+}
